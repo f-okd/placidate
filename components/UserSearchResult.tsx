e17a6a3cbe66ee199274acda25c5b4d9c2a5932c
Faye <oluwafayefunmiokude@gmail.com>
2025-02-16 01:04:45 +0000

    ♻️refactor: Reorganise UserSearchResult component

--- components/UserSearchResult.tsx
+++ components/UserSearchResult.tsx
@@ -8,6 +8,8 @@ interface IUserSearchResult {
   username: string;
   avatarUrl: string | null;
   router: Router;
+  isOnFollowingList?: boolean;
+  isOnFollowerList?: boolean;
 }
 
 export default function UserSearchResult({
@@ -26,26 +28,24 @@ export default function UserSearchResult({
     }
   };
   return (
-    <View className='flex-row gap 2'>
-      <TouchableOpacity
-        testID='search-result'
-        className='flex-row items-center p-2'
-        onPress={() => navigateToProfile()}
-      >
-        <Image
-          testID='avatar'
-          source={
-            avatarUrl
-              ? { uri: avatarUrl }
-              : require('@/assets/images/default-avatar.jpg')
-          }
-          style={profilePictureImageStyle}
-        />
-        <Text testID='username' className='p-2 font-bold'>
-          {username}
-        </Text>
-      </TouchableOpacity>
-    </View>
+    <TouchableOpacity
+      testID='search-result'
+      className='flex-row items-center p-2'
+      onPress={() => navigateToProfile()}
+    >
+      <Image
+        testID='avatar'
+        source={
+          avatarUrl
+            ? { uri: avatarUrl }
+            : require('@/assets/images/default-avatar.jpg')
+        }
+        style={profilePictureImageStyle}
+      />
+      <Text testID='username' className='p-2 font-bold'>
+        {username}
+      </Text>
+    </TouchableOpacity>
   );
 }
 
