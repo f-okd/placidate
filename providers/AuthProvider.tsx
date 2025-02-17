4378d29091176a343d4a9773d146bf20a7ae035f
Faye <oluwafayefunmiokude@gmail.com>
2025-02-17 19:44:44 +0000

    ✨ feat: Show toast for missing and invalid credentials

--- providers/AuthProvider.tsx
+++ providers/AuthProvider.tsx
@@ -1,8 +1,10 @@
 import { supabase } from '@/lib/supabase/client';
 import { Database } from '@/lib/supabase/types';
 import SupabaseUserEndpoint from '@/lib/supabase/UserEndpoint';
+import { showToast } from '@/utils/helpers';
 import { useRouter } from 'expo-router';
 import { createContext, ReactNode, useContext, useState } from 'react';
+import * as EmailValidator from 'email-validator';
 
 export type Profile = Database['public']['Tables']['profiles']['Row'];
 
@@ -37,11 +39,18 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const userEndpoint = new SupabaseUserEndpoint();
 
   const signIn = async (email: string, password: string) => {
+    if (!email) return showToast('Missing email');
+    if (!password) return showToast('Missing password');
+
+    if (!EmailValidator.validate(email)) return showToast('Invalid email');
+
     const { data, error } = await supabase.auth.signInWithPassword({
       email,
       password,
     });
 
+    if (error?.message == 'Invalid login credentials')
+      return showToast('Invalid login credentials');
     if (error) return console.error(error);
 
     const profile = await userEndpoint.getProfile(data.user.id);
