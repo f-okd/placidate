084dd633085fdabccc9e3a4ae330b482b783a8ba
Faye <oluwafayefunmiokude@gmail.com>
2025-03-13 01:21:13 +0000

    refactor ♻️: Remove commenteed out getRecommendedPosts method

--- lib/supabase/PostEndpoint.ts
+++ lib/supabase/PostEndpoint.ts
@@ -177,68 +177,6 @@ class SupabasePostEndpoint {
     }
   }
 
-  /*
-  async getRecommendedPosts(
-    currentlyAuthenticatedUser: string
-  ): Promise<TGetHomePagePost[] | null> {
-    try {
-      // First, get the blocked relationships
-      const { data: blocks, error: blocksError } = await supabase
-        .from('blocks')
-        .select('blocker_id, blocked_id');
-
-      if (blocksError) {
-        console.error('Error fetching blocks:', blocksError);
-        return null;
-      }
-
-      // Get all blocked and blocker IDs related to the current user
-      const blockedUsers = new Set(
-        blocks
-          ?.filter((block) => block.blocker_id === currentlyAuthenticatedUser)
-          .map((block) => block.blocked_id)
-      );
-      const blockedByUsers = new Set(
-        blocks
-          ?.filter((block) => block.blocked_id === currentlyAuthenticatedUser)
-          .map((block) => block.blocker_id)
-      );
-
-      // Get posts
-      const { data: posts, error: postsError } = await supabase
-        .from('posts')
-        .select(
-          `
-        *,
-        profiles!posts_author_id_fkey(id, username, avatar_url),
-        post_tags(
-          tag_id,
-          tags(
-            name
-          )
-        )
-      `
-        )
-        .order('created_at', { ascending: false });
-
-      if (postsError) {
-        console.error('Error fetching posts:', postsError);
-        return null;
-      }
-
-      // Filter out posts from blocked users
-      return posts.filter(
-        (post) =>
-          !blockedUsers.has(post.author_id) &&
-          !blockedByUsers.has(post.author_id)
-      );
-    } catch (error) {
-      console.error('Error in getPosts:', error);
-      return null;
-    }
-  }
-*/
-
   async searchForPosts(
     currentlyAuthenticatedUser: string,
     searchTerm: string
