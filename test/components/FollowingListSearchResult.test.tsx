4a8a5bf288995e9ecf07056839e44aeaea53b503
Faye <oluwafayefunmiokude@gmail.com>
2025-03-16 16:21:12 +0000

    test 🧪: Add tests for null and set avatar in follower and following search results

--- test/components/FollowingListSearchResult.test.tsx
+++ test/components/FollowingListSearchResult.test.tsx
@@ -25,7 +25,7 @@ const mockProps = {
 };
 
 describe('FollowingListSearchResult', () => {
-  it('successfully renders component with correct information', () => {
+  it('successfully renders component with correct information (null avatar)', () => {
     render(<FollowingListSearchResult {...mockProps} />);
 
     expect(screen.getByTestId('search-result')).toBeTruthy();
@@ -34,11 +34,32 @@ describe('FollowingListSearchResult', () => {
 
     expect(screen.queryByTestId('unfollow-button')).toBeNull();
   });
+
+  it('successfully renders component with correct information (set avatar)', () => {
+    const mockProps = {
+      profile: {
+        ...mockProfile,
+        avatar_url: 'https://via.placeholder.com/150/92c952',
+      },
+      router: mockRouter,
+      onUnfollow: mockUnfollow,
+      ownList: false,
+    };
+    render(<FollowingListSearchResult {...mockProps} />);
+
+    expect(screen.getByTestId('search-result')).toBeTruthy();
+    expect(screen.getByTestId('avatar')).toBeTruthy();
+    expect(screen.getByTestId('username')).toHaveTextContent('test user');
+
+    expect(screen.queryByTestId('unfollow-button')).toBeNull();
+  });
+
   it('shows unfollow button when user is viewing their own list', () => {
     render(<FollowingListSearchResult {...{ ...mockProps, ownList: true }} />);
 
     expect(screen.getByTestId('unfollow-button')).toBeTruthy();
   });
+
   it('calls the onUnfollow prop function when the unfollow button is pressed', () => {
     render(<FollowingListSearchResult {...{ ...mockProps, ownList: true }} />);
 
